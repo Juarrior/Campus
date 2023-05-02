a7beb077d864846adec6aab05a7b05c976db3010
Juarrior <juanreig197@gmail.com>
2023-05-02 17:46:27 +0200

    Prueba cambios chakra

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import * as React from 'react'
+
+import { ChakraProvider, Container, Heading } from '@chakra-ui/react'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-
       <header class="header">
        <a href="#" class="logo">Campus EVO</a>
        <nav class="navbar">
@@ -21,12 +23,12 @@ function App() {
       </header>
 
       <section class="home">
-        <div class="home-content">
-          <h1>Comienza a crecer</h1>
+        <Container class="home-content">
+          <Heading>Comienza a crecer</Heading>
           <h3>Cursos gratuitos</h3>
-          <h3>Para cualquier nivel</h3>
-          <p></p>
-        </div>
+          
+          
+        </Container>
       </section>
 
       
